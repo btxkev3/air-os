b117b7f1b111a87e2d3f2801db760b2755ded973
Kevin <kevinchanxy@gmail.com>
2022-07-11 17:21:11 +0000

    added pages

---
+++ data/sidebardata.tsx
@@ -0,0 +1,8 @@
+export const sidebarData = [
+    {
+        link: "/"
+    },
+    {
+        link: "/settings"
+    }
+]
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Sidebar from '../components/Sidebar'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div className="flex h-screen">
+    <Sidebar />
+    <Component {...pageProps} />
+  </div>
 }
 
 export default MyApp
